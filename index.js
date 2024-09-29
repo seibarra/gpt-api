1ae95651b4d3d5fc83152089becd5840dc111489
Sebastián Ibarra <sebastianibarra104@gmail.com>
2024-09-29 19:29:48 -0300

    cors open

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function checkingStatus(res, threadId, runId) {
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'upsupra.com'); 
+  res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
