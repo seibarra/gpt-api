d583b6c5c37eda1772931872a6aa799bfe759320
Sebastián Ibarra <sebastianibarra104@gmail.com>
2024-09-30 14:16:43 -0300

    log added

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ async function checkingStatus(res, apiKey, threadId, runId) {
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
+  console.log('Request received');
+  
   res.header('Access-Control-Allow-Origin', '*'); 
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
